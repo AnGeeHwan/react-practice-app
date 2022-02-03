da17fa2308e1ae57a8e4fc23d24c1f29c427f1bf
AGH <rlghks9252@gmail.com>
2022-02-03 17:36:34 +0900

    react study day 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    { id: 'e1', title: 'Car Insurance1', amount: 221.31, date: new Date(2021, 3, 28) },
+    { id: 'e2', title: 'Car Insurance2', amount: 11.31, date: new Date(2021, 3, 28) },
+    { id: 'e3', title: 'Car Insurance3', amount: 2231.1, date: new Date(2021, 3, 28) },
+    { id: 'e4', title: 'Car Insurance4', amount: 331.33, date: new Date(2021, 3, 28) },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
