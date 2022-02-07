04541674241a5a3b70b413ba5a9934d436124f8d
AGH <rlghks9252@gmail.com>
2022-02-07 17:58:13 +0900

    useState 사용하여 InputForm 컨트롤 기능 완료! state컨트롤 연습!

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const App = () => {
-  const expenses = [
-    { id: 'e1', title: 'Car Insurance1', amount: 221.31, date: new Date(2021, 3, 10) },
-    { id: 'e2', title: 'Car Insurance2', amount: 11.31, date: new Date(2021, 3, 15) },
-    { id: 'e3', title: 'Car Insurance3', amount: 2231.1, date: new Date(2021, 3, 12) },
-    { id: 'e4', title: 'Car Insurance4', amount: 331.33, date: new Date(2021, 3, 25) },
-    { id: 'e5', title: 'Car Insurance5', amount: 31.3344, date: new Date(2021, 3, 21) },
-  ];
+import './App.css'
 
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
+const Dummy_expenses = [
+  { id: 'e1', title: 'Car Insurance1', amount: 221.31, date: new Date(2021, 3, 10) },
+  { id: 'e2', title: 'Car Insurance2', amount: 11.31, date: new Date(2021, 3, 15) },
+  { id: 'e3', title: 'Car Insurance3', amount: 2231.1, date: new Date(2021, 3, 12) },
+  { id: 'e4', title: 'Car Insurance4', amount: 331.33, date: new Date(2021, 3, 25) },
+  { id: 'e5', title: 'Car Insurance5', amount: 31.3344, date: new Date(2021, 3, 21) },
+];
+
+
+const App = () => {
+
+  const [expenses, setExpenses] = useState(Dummy_expenses);
+  const [txtBtn, setTxtBtn] = useState("add");
+  const [addExpense, setAddExpense] = useState(false);
+
+  const addExpenseHandler = expense => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  }
+
+  const exportExpense = () => {
+    return addExpense === false ? <></> :
+      <NewExpense onAddExpense={addExpenseHandler} />
+  }
+
+  const txtChangehandler = () => {
+    return txtBtn === "add" ? setTxtBtn("cancle") : setTxtBtn("add");
+  }
+
 
   return (
-    <div>
-      <NewExpense />
+    <div className='App'>
+      {exportExpense()}
       <Expenses items={expenses} />
+      <button className='btnAdd' onClick={() => {
+        setAddExpense(!addExpense);
+        txtChangehandler();
+      }}>{txtBtn}</button>
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,35 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expense = (props) => {
-  const ExpenseList = props.items.map((it) => (
-    <ExpenseItem key={it.id} title={it.title} amount={it.amount} date={it.date} />
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filetrExpenses = props.items.filter((it) => it.date.getFullYear() === parseInt(filteredYear));
+
+  const ExpenseList = filetrExpenses.map((it) => (
+    <ExpenseItem
+      key={it.id}
+      title={it.title}
+      amount={it.amount}
+      date={it.date} />
   ));
 
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
+
+
   return (
-    <Card className="expenses">
-      {ExpenseList}
-      {/* <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date} />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date} />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date} />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date} /> */}
-    </Card>
+    <>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        {ExpenseList}
+      </Card>
+    </>
   )
 }
 
